99d952e5b66c4ceb0ae88bb5f2ff5351232b8a38
Szymon Murawski <szymon.murawski@gmail.com>
2020-11-25 15:12:30 +0100

    Reading input

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -6,7 +6,24 @@ namespace TicTacToe
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("State of the first field: ");
+            string field1 = Console.ReadLine();
+            Console.Write("State of the second field: ");
+            string field2 = Console.ReadLine();
+            Console.Write("State of the third field: ");
+            string field3 = Console.ReadLine();
+            Console.Write("State of the fourth field: ");
+            string field4 = Console.ReadLine();
+            Console.Write("State of the fifth field: ");
+            string field5 = Console.ReadLine();
+            Console.Write("State of the sixth field: ");
+            string field6 = Console.ReadLine();
+            Console.Write("State of the seventh field: ");
+            string field7 = Console.ReadLine();
+            Console.Write("State of the eight field: ");
+            string field8 = Console.ReadLine();
+            Console.Write("State of the nine field: ");
+            string field9 = Console.ReadLine();
         }
     }
 }
