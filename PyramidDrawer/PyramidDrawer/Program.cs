6f6ff172352a6cef256d019b12d7a1810057936e
Szymon Murawski <szymon.murawski@gmail.com>
2020-12-02 15:47:00 +0100

    Handling user input

--- PyramidDrawer/PyramidDrawer/Program.cs
+++ PyramidDrawer/PyramidDrawer/Program.cs
@@ -4,9 +4,12 @@ namespace PyramidDrawer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to program that will draw a pyramid!");
+            Console.Write("What should be the height of the pyramid? ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Drawing a pyramid of height {height}");
         }
     }
 }
