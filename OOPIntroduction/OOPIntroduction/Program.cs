cc55b4e44e7b42596d53d9d09d988fcb00c75ec6
Szymon Murawski <szymon.murawski@gmail.com>
2021-01-20 13:33:08 +0100

    Simple reading

--- OOPIntroduction/OOPIntroduction/Program.cs
+++ OOPIntroduction/OOPIntroduction/Program.cs
@@ -4,9 +4,26 @@ namespace OOPIntroduction
 {
     class Program
     {
-        static void Main(string[] args)
+        // Ask the user for a name of a person, END to finish
+        // After finish, display the list of users
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] People = new string[20];
+            Console.WriteLine("Provide names of users, type END to finish and display this list");
+            for(int i =0; i < People.Length; i++)
+            {
+                Console.WriteLine($"{i+1} Person: ");
+                string name = Console.ReadLine();
+                if (name == "END")
+                {
+                    break;
+                }
+                People[i] = name;
+            }
+            for(int i = 0; i < People.Length; i++)
+            {
+                Console.WriteLine(People[i]);
+            }
         }
     }
 }
